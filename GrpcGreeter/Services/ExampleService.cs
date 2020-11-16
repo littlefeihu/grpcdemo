967585b8c7a966c151e5cfe3d762462e6613ab2d
longbao@7net.cc <longbao@7net.cc>
2020-11-16 16:55:48 +0800

    1

---
+++ GrpcGreeter/Services/ExampleService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcExample;
+
+namespace GrpcGreeter.Services
+{
+    public class ExampleService : ExampleService_Pro.ExampleService_ProBase
+    {
+        //一元方法
+        public override Task<ExampleResponse> UnaryCall(ExampleRequest request, ServerCallContext context)
+        {
+            var response = new ExampleResponse() { Message = "UnaryCall" };
+            return Task.FromResult(response);
+        }
+        //服务器流式处理方法
+        public override async Task StreamingFromServer(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                await responseStream.WriteAsync(new ExampleResponse { Message = i.ToString() });
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+        //客户端流式处理方法
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var message = requestStream.Current;
+                // ...
+            }
+            return new ExampleResponse();
+        }
+        //双向流式处理方法
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream,
+    IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            // Read requests in a background task.
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    // Process request.
+                }
+            });
+
+            // Send responses until the client signals that it is complete.
+            while (!readTask.IsCompleted)
+            {
+                await responseStream.WriteAsync(new ExampleResponse());
+                await Task.Delay(TimeSpan.FromSeconds(1), context.CancellationToken);
+            }
+        }
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using GrpcExample;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+           // var client = new Greeter.GreeterClient(channel);
+            var client1 = new GrpcExample.ExampleService_Pro.ExampleService_ProClient(channel);
+
+          //  var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+            var reply1 = await client1.UnaryCallAsync(new ExampleRequest {  PageSize=1 });
+
+            Console.WriteLine("Greeting: " + reply1.Message);
+
+
+            var response= client1.StreamingFromServer(new ExampleRequest());
+            CancellationToken cancellationToken;
+            while (await response.ResponseStream.MoveNext(cancellationToken))
+            {
+                var message = response.ResponseStream.Current.Message;
+                Console.WriteLine("StreamingFromServer:"+message);
+            }
+
+
+            Console.WriteLine("Greeting: " + response.ResponseStream);
+
+           // Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
